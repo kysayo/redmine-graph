0bae93b1ab35a52024f9e616dd99e97bbce499cd
kisayo <saitouky2710@gmail.com>
2026-02-25 17:17:53 +0900

    fix: process is not defined エラーを修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
   build: {
     lib: {
       entry: 'src/main.tsx',
